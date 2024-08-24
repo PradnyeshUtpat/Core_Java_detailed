0f1c9e596af7b783715590bcda8ee2904dc3aded
PradnyeshUtpat <pradneshputpat@gmail.com>
2024-08-24 18:37:32 +0530

    Added Arithmetic Ops

---
+++ Arithmetic_Operators.java
@@ -0,0 +1,24 @@
+public class Arithmetic_Operators {
+    public static void main(String[] args) {
+        
+        System.out.println("Arithmetic Operators");
+        int n1 = 5;
+        int n2 = 6;
+        System.out.println(n1+n2);
+
+        int n3 = 8;
+        int n4 = 4;
+        System.out.println(n3-n4);
+
+        int n5 = 7;
+        int n6 = 8;
+        System.out.println(n5*n6);
+
+        int n7 = 10;
+        int n8 = 4;
+        System.out.println(n7/n8);//use % for remainder because / gives u quotient only
+
+        System.out.println(5>6);
+    }
+    
+}
