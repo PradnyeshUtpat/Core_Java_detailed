5d2e2bfd3149c6c0b1e48935ad77dbb490f58e4d
PradnyeshUtpat <pradneshputpat@gmail.com>
2024-08-24 18:33:33 +0530

    Added Variables

---
+++ Variables.java
@@ -0,0 +1,30 @@
+public class Variables {
+    public static void main(String args[]) {
+        int num;
+        num = 3;
+        double marks = 1.4d;
+        short  p_ = 1;
+        float p = 6.0f;
+        long l = 10000l;
+        String z;
+        z = "hi buddy";
+        System.out.println(z);
+        System.out.println(p);
+        System.out.println(marks);
+        System.out.println(num);
+        System.out.println(num + marks + p + p_);
+        System.out.println(l);
+        boolean b = true;
+        System.out.println(b);
+        char c = 'k';
+        System.out.println(c);
+        //byte , short , int , long 
+        //double , float
+        //boolean
+        //char
+    }
+}
+
+
+//values of variables are called literals
+
