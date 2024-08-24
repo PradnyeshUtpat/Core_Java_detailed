dcab25e0ec43d67a254edbcee1fc30747a8311b6
PradnyeshUtpat <pradneshputpat@gmail.com>
2024-08-24 18:34:06 +0530

    Added Literals

---
+++ Literals.java
@@ -0,0 +1,15 @@
+public class Literals {
+    public static void main(String[] args) {
+        int num = 10_00_000;
+        double num1 = 1e11;
+        int num2 = 0b101;
+        int num3 = 0xFFF;
+        char c = 'a';
+        c++;
+        System.out.println(c);
+        System.out.println(num);
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+    }
+}
