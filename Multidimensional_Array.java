db122e97efcdae1d3ca17e91ff3d2fe5d9384b43
PradnyeshUtpat <pradneshputpat@gmail.com>
2025-02-08 20:03:49 +0530

    Added Multidimensional Array

--- Multidimensional_Array.java
+++ Multidimensional_Array.java
@@ -1,28 +1,29 @@
 public class Multidimensional_Array {
     public static void main(String[] args) {
 
-        int matrix[][] = new int[3][3];  
+        int[][] matrix = new int[3][3];
         
-        int anotherMatrix[][] = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {10, 11, 12} };
+        int[][] anotherMatrix = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {10, 11, 12} };
         
         int element = anotherMatrix[1][2];
         int c = anotherMatrix[0].length;
         int r = anotherMatrix.length;
 
-        
 
-        for(int i = 0 ; i < r ; i++){
-            for(int j = 0 ; j < c ; j++){
-                System.out.print(anotherMatrix[i][j] + " ");
+        for (int[] ints : anotherMatrix) {
+            for (int j = 0; j < c; j++) {
+                System.out.print(ints[j] + " ");
             }
             System.out.println();
         }
 
-        // for(int i[] : anotherMatrix){
-        //     for(int j : i){
-        //         System.out.println(j);
-        //     }
-        // }
+         for(int[] i : matrix){
+             for(int j : i){
+                 j = (int) (Math.random()*10);
+                 System.out.print(j + " ");
+             }
+             System.out.println();
+         }
 
     }
 }
