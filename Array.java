3f948bd283ef66b125e867ea2864c053902eb572
PradnyeshUtpat <pradneshputpat@gmail.com>
2025-02-08 19:57:48 +0530

    Added arrays and string

--- Array.java
+++ Array.java
@@ -1,16 +1,16 @@
 public class Array {
     public static void main(String[] args) {
-        int numbers[] = new int[5];
-        //By default all the values are 0
-        int primeNumbers[] = {2, 3, 5, 7, 11};
+        int[] numbers = new int[5];
+        //By default, all the values are 0
+        int[] primeNumbers = {2, 3, 5, 7, 11};
 
         int firstNumber = numbers[0];
         int thirdPrime = primeNumbers[2];
         System.out.println(firstNumber);
         int length = numbers.length;
 
-        for (int i = 0; i < primeNumbers.length; i++) {
-            System.out.println(primeNumbers[i]);
+        for (int primeNumber : primeNumbers) {
+            System.out.println(primeNumber);
         }
 
         for (int number : primeNumbers) {
--- String_.java
+++ String_.java
@@ -1,6 +1,8 @@
 public class String_ {
     public static void main(String[] args) {
         String name = "Pradnyesh"; // We will be using this in this tutorial
+        String name2 = "Pradnyesh";
+
         String n = new String(); // this is what actually happens behind the scenes
         n = "Prashant";
         System.out.println(n); 
@@ -8,7 +10,11 @@ public class String_ {
         System.out.println(n.charAt(3));
         System.out.println(n.endsWith("esh")); 
         System.out.println(n.startsWith("Pras"));
-        name = name.concat(" Utpat");
+        //name = name.concat(" Utpat");
         System.out.println(name);
+        System.out.println(name2);
+        System.out.println(System.identityHashCode(name));
+        System.out.println(System.identityHashCode(name2));
+
     }
 }
