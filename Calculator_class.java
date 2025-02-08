69bfbe7b5e5ad5756afc3ff377865e073694ad4b
PradnyeshUtpat <pradneshputpat@gmail.com>
2025-02-08 18:34:46 +0530

    Added constructor

--- Calculator_class.java
+++ Calculator_class.java
@@ -4,10 +4,15 @@ public class Calculator_class {
         int num2 = 5;
         Calculator calc = new Calculator();
         calc.add(num1,num2);
+        calc.a = 11;
+        System.out.println(calc.a);
     }
 }
 class Calculator{
-    int a;
+    Calculator() {
+        a = 10;
+    }
+    static int a;
     public int add(int n1 , int n2){
         int r = n1+n2;
         return r;
