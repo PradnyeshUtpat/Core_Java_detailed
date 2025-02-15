a2553f94e6a8bd8c8b125311a311b1b621b37be6
PradnyeshUtpat <pradneshputpat@gmail.com>
2025-02-15 23:49:51 +0530

    Added FinalKeyword

---
+++ FinalKeyword.java
@@ -0,0 +1,23 @@
+// final keyword can be - variable , method , class
+public class FinalKeyword {
+    public static void main(String[] args) {
+        int num = 9; // ------------> if made final no one can modify it
+        num = 10;
+        System.out.println(num);
+    }
+}
+
+class P{ // ----------------> if made final no one can extend it
+    public void show(){ // ------------> if made final no one can override it
+        System.out.println("In P show");
+    }
+    public void add(int a , int b){
+        System.out.println(a+b);
+    }
+}
+class C extends P{
+    public void show(){
+        System.out.println("In C show");
+    }
+}
+
