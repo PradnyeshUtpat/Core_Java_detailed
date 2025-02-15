08face4af1d602632264a4658bb0d16bfb3b5aef
PradnyeshUtpat <pradneshputpat@gmail.com>
2025-02-15 12:28:43 +0530

    Added Inheritance

--- Multilevel_Inheritance.java
+++ Multilevel_Inheritance.java
@@ -1,5 +1,5 @@
-public class Inheritance {
-    public static void main(String a[])
+public class Multilevel_Inheritance {
+    public static void main(String[] args)
     {
         //Calc obj=new Calc();
         AdvCalc obj=new AdvCalc();
@@ -7,16 +7,16 @@ public class Inheritance {
         int r2=obj.sub(7,3);
         int r3=obj.multi(5,3);
         int r4=obj.div(15,4);
-        System.out.println(obj.num);
-        System.out.println(r1+" "+r2);
+        obj.num = 1;
 
+        System.out.println(r1+" "+r2);
 
     }
 }
 
-class Calc
+class Calc1
 {
-    public int num = 10;
+    int num;
     public int add(int n1, int n2)
     {
         return n1+n2;
@@ -28,9 +28,8 @@ class Calc
 }
 
 
-class AdvCalc extends Calc
+class AdvCalc1 extends Calc
 {
-
     public int multi(int n1, int n2)
     {
         return n1*n2;
