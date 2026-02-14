95709989ffd54252f8b0d29acf59092e7c512e53
Pradnyesh Utpat <pradnyeshutpat@Pradnyeshs-MacBook-Air.local>
2026-02-14 23:15:05 +0530

    added modifs

--- Type_conversion_Casting.java
+++ Type_conversion_Casting.java
@@ -1,7 +1,7 @@
 public class Type_conversion_Casting {
     public static void main(String[] args) {
         float f = 9.9f;
-        int i = (int) f; // implicit - type conversion
+        int i = (int) f; // explicit - type conversion
         System.out.println(i);
 
         //Type casting
@@ -17,6 +17,10 @@ public class Type_conversion_Casting {
         byte s = 10;
         byte t = 30;
         int res = t*s;
+        long ans = 100000000L;
+        int b1 = 450;
+        ans = b1;
+        System.out.println(ans);
         System.out.println(res); //Type promotion
     }
 }
