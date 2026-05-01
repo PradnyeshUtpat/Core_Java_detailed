77da3de4c5a2bc2950524fb667813f4f21a81e01
Pradnyesh Utpat <pradnyeshutpat@Pradnyeshs-MacBook-Air.local>
2026-05-01 21:43:51 +0530

    added test cases

--- Type_conversion_Casting.java
+++ Type_conversion_Casting.java
@@ -1,14 +1,14 @@
 public class Type_conversion_Casting {
     public static void main(String[] args) {
         float f = 9.9f;
-        int i = (int) f; // explicit - type conversion
+        int i = (int) f; // explicit type casting - converting float to int, fractional part will be lost
         System.out.println(i);
 
-        //Type casting
+
 
         byte b = 127;
         int a = 257 ;
-        // a = b;
+        // a = b;  // implicit type conversion - converting byte to int, no data loss
         // System.out.println(a);
         b = (byte) a; //explicit - type casting 257%rangeOfTypeToBeConvertedTo = 257%256 = 1
         System.out.println(b);
