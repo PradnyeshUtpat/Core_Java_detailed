6d7b1b71afee11394bc50ed2e7cccf87aa84b491
PradnyeshUtpat <pradneshputpat@gmail.com>
2026-05-02 17:10:09 +0530

    added immutable string concept

--- Mutable_Immutable_String.java
+++ Mutable_Immutable_String.java
@@ -2,8 +2,12 @@ public class Mutable_Immutable_String {
     public static void main(String[] args) {
         String s1 = "Pradnyesh";
         String s2 = "Pradnyesh";
+        System.out.println(s1.hashCode());
+        System.out.println(s2.hashCode());
         s1 = s1 + " Utpat";
         System.out.println(s1==s2);
+        System.out.println(s1.hashCode());
+        System.out.println(s2.hashCode());
 
         /* 
             What exactly is happening behind the scenes?
